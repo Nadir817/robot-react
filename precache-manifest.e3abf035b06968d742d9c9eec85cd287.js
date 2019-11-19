05327237ee244c482a421a0c8b151bec12b17ab2
Nadir817 <nadir.erkin@gmail.com>
2019-11-19 17:16:15 -0500

    Updates

---
+++ precache-manifest.e3abf035b06968d742d9c9eec85cd287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b60ce38979e7bf6060ed45dbf80c4d1",
+    "url": "/robot-react/index.html"
+  },
+  {
+    "revision": "ef9c38032067613cb1c7",
+    "url": "/robot-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "31a957efd2a5c2ab22fa",
+    "url": "/robot-react/static/css/main.7986097b.chunk.css"
+  },
+  {
+    "revision": "ef9c38032067613cb1c7",
+    "url": "/robot-react/static/js/2.9eb98899.chunk.js"
+  },
+  {
+    "revision": "31a957efd2a5c2ab22fa",
+    "url": "/robot-react/static/js/main.f88dec70.chunk.js"
+  },
+  {
+    "revision": "91fdae7c7562b82532b0",
+    "url": "/robot-react/static/js/runtime-main.cbfb3e7b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
